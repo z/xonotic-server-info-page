eda1011436187a3d8272da9416eeeca5f52f2346
z <z@xnz.me>
2016-01-09 18:38:48 -0500

    style redux, different placeholder image, more maps

--- config/manifest.js
+++ config/manifest.js
@@ -3,7 +3,13 @@ var manifest = {
     // list of bsp names
     mapList: [
         'vinegar_v3',
-        'dance_nex'
+        'dance_nex',
+        'accident_v3',
+        'battlevalentine',
+        'furious',
+        'gforce2',
+        'got_wood',
+        'gothic_block'
     ],
 
     // list the posts you'd like to show here
